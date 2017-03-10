9eca7905741b4436db301834660b182bf6eb58e3
dothiko <dothiko@gmail.com>
2017-03-10 23:02:53 +0900

    rulercontroller.py: Sorry, I forgot to add this!
    I'm sorry, I forgot to git-add gui/rulercontroller.py.

---
+++ gui/rulercontroller.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from gui.linearcontroller import *
+import gui.style
+
+class RulerNode(object):
+
+    color = gui.style.EDITABLE_ITEM_COLOR
+
+    def __init__(self, linear_pos):
+        self.linear_pos = linear_pos
+
+
+class RulerController(LinearController):
+    """RulerController, to manage & show parallel ruler settings
+    in parallelruler assistant of freehand mode.
+    """
+    ACTION_NAME = "FreehandMode"
+
+    def is_ready(self):
+        # Does not use self.nodes in this class.
+        return (self._start_pos is not None 
+                and self._end_pos is not None)
+
+    def set_start_pos(self, tdw, disp_pos):
+        super(RulerController, self).set_start_pos(tdw, disp_pos)
+        if len(self.nodes) < 1:
+            self.nodes.append(RulerNode(0.0))
+
+    def set_end_pos(self, tdw, disp_pos):
+        super(RulerController, self).set_end_pos(tdw, disp_pos)
+        if len(self.nodes) < 2:
+            self.nodes.append(RulerNode(1.0))
+
+    def _shading_contents(self, cr, mode, tdw):
+        # base shading, considering for alpha transparency.
+        cr.save()
+        cr.set_source_rgb(1.0, 1.0, 1.0) 
+        cr.stroke()
+
+        # Drawing simurated gradiation
+        if len(self.nodes) > 0:
+
+            # Drawing nodes.
+            # if do this above 'simurated gradiation' loop,
+            # some lines overdraw node chips.
+            # so do that here to overdraw simulated lines.
+            for i, pt, cx, cy, ex, ey in self._enum_node_position(tdw):
+                self._draw_single_node(cr, ex, ey, i, pt)
+
+        cr.restore()
+
+if __name__ == '__main__':
+
+    pass
+
+
